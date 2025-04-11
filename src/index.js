6304e64235ad58d17bd40c3de7c2cec57ef6b8bd
AbinsEagle <41841495+AbinsEagle@users.noreply.github.com>
2025-04-11 13:33:43 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import ExamTimerApp from './ExamTimerApp';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ExamTimerApp />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
